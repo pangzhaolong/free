9cc556139692070f4f922777881c9937c9a768fe
Aaron <68183741@qq.com>
2021-10-18 21:25:28 +0800

    [修改] 首页数据展示

--- business/module-home/src/main/java/com/donews/home/model/TopModel.java
+++ business/module-home/src/main/java/com/donews/home/model/TopModel.java
@@ -1,16 +1,21 @@
 package com.donews.home.model;
 
+import android.os.SystemClock;
+
 import androidx.lifecycle.MutableLiveData;
 
 import com.donews.base.model.BaseLiveDataModel;
 import com.donews.home.BuildConfig;
 import com.donews.home.api.HomeApi;
 import com.donews.home.bean.DataBean;
+import com.donews.home.bean.RealTimeBean;
+import com.donews.home.bean.SecKilBean;
 import com.donews.home.bean.TopGoodsBean;
 import com.donews.network.EasyHttp;
 import com.donews.network.cache.model.CacheMode;
 import com.donews.network.callback.SimpleCallBack;
 import com.donews.network.exception.ApiException;
+import com.donews.utilslibrary.utils.DateTimeUtils;
 
 /**
  * <p> </p>
@@ -62,6 +67,48 @@ public class TopModel extends BaseLiveDataModel {
                     @Override
                     public void onSuccess(TopGoodsBean dataBean) {
                         mutableLiveData.postValue(dataBean);
+                    }
+                });
+
+        return mutableLiveData;
+    }
+
+    public MutableLiveData<RealTimeBean> getRealTimeData() {
+        MutableLiveData<RealTimeBean> mutableLiveData = new MutableLiveData<>();
+
+        EasyHttp.get(HomeApi.realTimeUrl + "?rank_type=1&page_size=2")
+                .cacheMode(CacheMode.NO_CACHE)
+                .execute(new SimpleCallBack<RealTimeBean>() {
+
+                    @Override
+                    public void onError(ApiException e) {
+                        mutableLiveData.postValue(null);
+                    }
+
+                    @Override
+                    public void onSuccess(RealTimeBean realTimeBean) {
+                        mutableLiveData.postValue(realTimeBean);
+                    }
+                });
+
+        return mutableLiveData;
+    }
+
+    public MutableLiveData<SecKilBean> getSecKilData() {
+        MutableLiveData<SecKilBean> mutableLiveData = new MutableLiveData<>();
+//        String roundTime = DateTimeUtils.getNow("yyyy-MM-dd HH:00:00");
+        EasyHttp.get(HomeApi.seckiltUrl + "?&page_size=2")
+                .cacheMode(CacheMode.NO_CACHE)
+                .execute(new SimpleCallBack<SecKilBean>() {
+
+                    @Override
+                    public void onError(ApiException e) {
+                        mutableLiveData.postValue(null);
+                    }
+
+                    @Override
+                    public void onSuccess(SecKilBean secKilBean) {
+                        mutableLiveData.postValue(secKilBean);
                     }
                 });
 
