7b1985bfa816463df2d32af5237d5a3793c394b9
Aaron <68183741@qq.com>
2021-10-26 16:32:23 +0800

    [更新] 增加埋点数据

--- common/library-utils/src/main/java/com/donews/utilslibrary/dot/Dot.java
+++ common/library-utils/src/main/java/com/donews/utilslibrary/dot/Dot.java
@@ -4,64 +4,64 @@ public class Dot {
     //    首页
     public final static String Page_Home = "Page_Home";
     //    晒单页
-    public final static String Page_ShowTime = "";
+    public final static String Page_ShowTime = "Page_ShowTime";
     //    中奖页
-    public final static String Page_Award = "";
+    public final static String Page_Award = "Page_Award";
     //    省钱购
-    public final static String Page_SaveMoneyBuy = "";
+    public final static String Page_SaveMoneyBuy = "Page_SaveMoneyBuy";
     //    个人中心
-    public final static String Page_UserCenter = "";
+    public final static String Page_UserCenter = "Page_UserCenter";
     //    开奖页
-    public final static String Page_Lottery = "";
+    public final static String Page_Lottery = "Page_Lottery";
     //    往期开奖页
-    public final static String Page_LotteryHistory = "";
+    public final static String Page_LotteryHistory = "Page_LotteryHistory";
     //    设置页
-    public final static String Page_Setting = "";
+    public final static String Page_Setting = "Page_Setting";
     //    意见反馈
-    public final static String Page_Feedback = "";
+    public final static String Page_Feedback = "Page_Feedback";
     //    关于我们
-    public final static String Page_AboutUs = "";
+    public final static String Page_AboutUs = "Page_AboutUs";
     //    账户注销
-    public final static String Page_AccountUnregister = "";
+    public final static String Page_AccountUnregister = "Page_AccountUnregister";
     //    中奖记录
-    public final static String Page_AwardRecords = "";
+    public final static String Page_AwardRecords = "Page_AwardRecords";
     //    参与记录
-    public final static String Page_PartInRecords = "";
+    public final static String Page_PartInRecords = "Page_PartInRecords";
     //    提现页
-    public final static String Page_Cash = "";
+    public final static String Page_Cash = "Page_Cash";
     //    中奖攻略
-    public final static String Page_WinDoc = "";
+    public final static String Page_WinDoc = "Page_WinDoc";
     //    详细规则
-    public final static String Page_DetailRule = "";
+    public final static String Page_DetailRule = "Page_DetailRule";
     //    联系客服
-    public final static String Page_ContactService = "";
+    public final static String Page_ContactService = "Page_ContactService";
     //    落地页
-    public final static String Page_Land = "";
+    public final static String Page_Land = "Page_Land";
     //    省钱攻略
-    public final static String Page_SaveMoneyDoc = "";
+    public final static String Page_SaveMoneyDoc = "Page_SaveMoneyDoc";
     //    首页
-    public final static String Btn_Home = "";
+    public final static String Btn_Home = "Btn_Home";
     //    晒单
-    public final static String Btn_ShowTime = "";
+    public final static String Btn_ShowTime = "Btn_ShowTime";
     //    开奖
-    public final static String Btn_Lottery = "";
+    public final static String Btn_Lottery = "Btn_Lottery";
     //    省钱购
-    public final static String Btn_SaveMoneyBuy = "";
+    public final static String Btn_SaveMoneyBuy = "Btn_SaveMoneyBuy";
     //    个人中心
-    public final static String Btn_UserCenter = "";
+    public final static String Btn_UserCenter = "Btn_UserCenter";
     //    立即抽奖
-    public final static String Btn_LotteryNow = "";
+    public final static String Btn_LotteryNow = "Btn_LotteryNow";
     //    分享
-    public final static String Btn_Share = "";
+    public final static String Btn_Share = "Btn_Share";
     //    购买
-    public final static String Btn_Buy = "";
+    public final static String Btn_Buy = "Btn_Buy";
     //    沾沾福气
-    public final static String Btn_BeBlessed = "";
+    public final static String Btn_BeBlessed = "Btn_BeBlessed";
     //    退出登录
-    public final static String Btn_Logout = "";
+    public final static String Btn_Logout = "Btn_Logout";
     //    换一个
-    public final static String Btn_Change1 = "";
+    public final static String Btn_Change1 = "Btn_Change1";
     //    立即抢购
-    public final static String Btn_BuyNow = "";
+    public final static String Btn_BuyNow = "Btn_BuyNow";
 
 }
