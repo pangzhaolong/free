058d7fd8fc8bbe923e72b2483a8df8875d55eee2
XuShuai <466911254@qq.com>
2021-10-28 11:27:03 +0800

    [更新] 1.广告SDK

---
+++ common/library-common/src/main/java/com/donews/common/bean/AppCommonConfig.kt
@@ -0,0 +1,13 @@
+package com.donews.common.bean
+
+import com.donews.common.contract.BaseCustomViewModel
+
+/**
+ *
+ * app 公共配置对象
+ * @author XuShuai
+ * @version v1.0
+ * @date 2021/10/28 11:18
+ */
+class AppCommonConfig : BaseCustomViewModel() {
+}
