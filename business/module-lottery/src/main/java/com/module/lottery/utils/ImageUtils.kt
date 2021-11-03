31652645e817a4e26b9516be429ddfd05636efa4
@he-gai <1223227664@qq.com>
2021-11-03 10:24:36 +0800

    [调整抽奖码弹窗时间]

--- business/module-lottery/src/main/java/com/module/lottery/utils/ImageUtils.kt
+++ business/module-lottery/src/main/java/com/module/lottery/utils/ImageUtils.kt
@@ -1,5 +1,6 @@
 package com.module.lottery.utils
 
+import android.app.Activity
 import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
@@ -13,11 +14,16 @@ class ImageUtils {
     companion object {
         @JvmStatic
         public fun setImage(context: Context, view: ImageView, src: String, roundingRadius: Int) {
-            var src: String? = src
-            src = UrlUtils.formatUrlPrefix(src)
-            val roundedCorners = RoundedCorners(roundingRadius)
-            val options = RequestOptions.bitmapTransform(roundedCorners)
-            Glide.with(context).load(src).apply(options).into(view)
+            if (context != null && context is Activity) {
+                if (context.isDestroyed) {
+                    return
+                }
+                var src: String? = src
+                src = UrlUtils.formatUrlPrefix(src)
+                val roundedCorners = RoundedCorners(roundingRadius)
+                val options = RequestOptions.bitmapTransform(roundedCorners)
+                Glide.with(context).load(src).apply(options).into(view)
+            }
         }
     }
 }
