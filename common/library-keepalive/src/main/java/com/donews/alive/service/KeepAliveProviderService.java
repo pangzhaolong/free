b9a55947966be73450b56e63ce44b2513e6d10a6
@he-gai <1223227664@qq.com>
2021-12-02 16:17:22 +0800

    [修复did not then call Service.startForeground()] .怀疑sdk方法中的doStartCommand 方法耗时了

--- common/library-keepalive/src/main/java/com/donews/alive/service/KeepAliveProviderService.java
+++ common/library-keepalive/src/main/java/com/donews/alive/service/KeepAliveProviderService.java
@@ -1,13 +1,14 @@
 package com.donews.alive.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
-
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.donews.alive.KeepAlive;
 import com.donews.alive.provider.IKeepAliveProvider;
 import com.keepalive.daemon.core.notification.NotifyResidentService;
-import com.orhanobut.logger.Logger;
-
 import java.util.List;
 
 /**
@@ -19,39 +20,46 @@ import java.util.List;
  */
 public class KeepAliveProviderService extends NotifyResidentService {
 
-	@Override
-	public void doStart() {
-		super.doStart();
-		List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
-		for (String path : mIKeepAliveProviderPathList) {
-			IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
-			if (provider != null) {
-				provider.doStart(this);
-			}
-		}
-	}
+    @Override
+    public void doStart() {
+        super.doStart();
+        List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
+        for (String path : mIKeepAliveProviderPathList) {
+            IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
+            if (provider != null) {
+                provider.doStart(this);
+            }
+        }
+    }
 
-	@Override
-	public void doStartCommand(Intent intent, int i, int i1) {
-		super.doStartCommand(intent, i, i1);
-		List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
-		for (String path : mIKeepAliveProviderPathList) {
-			IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
-			if (provider != null) {
-				provider.doStartCommand(this, intent, i, i1);
-			}
-		}
-	}
+    @Override
+    public void doStartCommand(Intent intent, int i, int i1) {
+        super.doStartCommand(intent, i, i1);
 
-	@Override
-	public void doRelease() {
-		super.doRelease();
-		List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
-		for (String path : mIKeepAliveProviderPathList) {
-			IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
-			if (provider != null) {
-				provider.doRelease(this);
-			}
-		}
-	}
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            nm.createNotificationChannel(new NotificationChannel("39223", "今日福利派送中", NotificationManager.IMPORTANCE_DEFAULT));
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "39223");
+            startForeground(39223, builder.build());
+        }
+        List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
+        for (String path : mIKeepAliveProviderPathList) {
+            IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
+            if (provider != null) {
+                provider.doStartCommand(this, intent, i, i1);
+            }
+        }
+    }
+
+    @Override
+    public void doRelease() {
+        super.doRelease();
+        List<String> mIKeepAliveProviderPathList = KeepAlive.getInstance().getIKeepAliveProviderPathList();
+        for (String path : mIKeepAliveProviderPathList) {
+            IKeepAliveProvider provider = (IKeepAliveProvider) ARouter.getInstance().build(path).navigation();
+            if (provider != null) {
+                provider.doRelease(this);
+            }
+        }
+    }
 }
